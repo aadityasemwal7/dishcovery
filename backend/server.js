f8f58d5fc19ab84903b225dceefe1d545cfce231
aadityasemwal7 <aadityasemwalbca2020@gmail.com>
2026-06-25 16:18:36 +0530

    feat(frontend): cors origin fixed

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
-const allowedOrigins = ["https://dish-covery-delta.vercel.app", "http://localhost:5173"];
+const allowedOrigins = ["https://dish-covery-delta.vercel.app", "http://localhost:5173", "https://dishcovery-jqimfl0pb-aadityas-projects-205f54df.vercel.app"];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
